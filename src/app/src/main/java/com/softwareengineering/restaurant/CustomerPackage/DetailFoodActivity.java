1b76dfffdb2b1a9580a8a48523dfad4c5e338ba6
nohiup <nohiup@gmail.com>
2024-01-12 15:50:34 +0700

    Fixed foodDetailActivity

--- src/app/src/main/java/com/softwareengineering/restaurant/CustomerPackage/DetailFoodActivity.java
+++ src/app/src/main/java/com/softwareengineering/restaurant/CustomerPackage/DetailFoodActivity.java
@@ -1,8 +1,17 @@
 package com.softwareengineering.restaurant.CustomerPackage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -12,14 +21,25 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.softwareengineering.restaurant.R;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.Locale;
 
 public class DetailFoodActivity extends AppCompatActivity {
     ImageView btn_back, image;
     TextView name, status, price, type, des, ingredients;
+
+    private Context context = DetailFoodActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +60,7 @@ public class DetailFoodActivity extends AppCompatActivity {
         Intent intent = getIntent();
         if (intent != null) {
             String foodName = intent.getStringExtra("foodName");
-            String foodImageUrl = intent.getStringExtra("foodImageUrl");
+            String foodImageRef = intent.getStringExtra("foodImageRef");
             boolean foodStatus = intent.getBooleanExtra("foodStatus", false);  // default value is false
             String foodPriceString = intent.getStringExtra("foodPrice");
             double foodPrice = Double.parseDouble(foodPriceString);
@@ -58,23 +78,108 @@ public class DetailFoodActivity extends AppCompatActivity {
             price.setText(formatPrice(foodPrice));
             type.setText(foodType);
 
-            int targetWidth = 380;
-            int targetHeight = 380;
-            int radius = 10;
-
-            RequestOptions requestOptions = new RequestOptions()
-                    .transforms(new CenterCrop(), new RoundedCorners(radius))
-                    .override(targetWidth, targetHeight);
-
-            Glide.with(this)
-                    .load(foodImageUrl)
-                    .apply(requestOptions)
-                    .into(image);
+            setImageFromReference(FirebaseStorage.getInstance().getReferenceFromUrl(foodImageRef), foodName, image);
         }
     }
 
     private String formatPrice(double price) {
         NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.getDefault());
         return numberFormat.format(price) + " VND";
+    }
+
+    public void setImageFromReference(StorageReference imgRef, String imageName, ImageView foodImgView){
+        File dataFolder = context.getDataDir();
+        File imgFile = new File(dataFolder, imageName+".jpg");
+        if (imgRef == null) {
+            Log.d("imgNameNull", imageName);
+            return;
+        }
+        if (!imgFile.exists()) {
+            //Load image from database
+            imgRef.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        //Load with Glide
+//                        Glide.with(context)
+//                                .load(task.getResult())
+//                                .diskCacheStrategy(DiskCacheStrategy.ALL) // Sử dụng DiskCacheStrategy.ALL để cache hình ảnh ở cả ổ đĩa và bộ nhớ
+//                                .into(foodImgView);
+                        //Download to cache:
+                        createCacheFile(imgFile, imgRef);
+                    }
+                    else {
+                        Log.e("Load image Task", "Failed");
+                    }
+                }
+            });
+        }
+
+        //Load image after done
+        Log.d("Error", imgFile.getAbsolutePath());
+        Bitmap img = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+        foodImgView.setImageBitmap(img);
+        // Kích thước cố định (ví dụ: 200x200 pixels)
+        int targetWidth = 380;
+        int targetHeight = 380;
+        int radius = 10;
+
+        RequestOptions requestOptions = new RequestOptions()
+                .transforms(new CenterCrop(), new RoundedCorners(radius))
+                .override(targetWidth, targetHeight);
+
+        Glide.with(this)
+                .load(img)
+                .apply(requestOptions)
+                .into(image);
+    }
+
+    //To create a cache file that will make the UI load faster than before
+    private void createCacheFile(File file, StorageReference imgRef){
+        File dataDir = context.getDataDir();
+        try {
+            // Ensure the directory exists or create it if it doesn't
+            if (!dataDir.exists()) {
+                dataDir.mkdirs(); // Create directory and its parent directories if not existing
+            }
+            Log.d("Error", file.getAbsolutePath());
+            file.createNewFile(); // Create
+
+        }catch (IOException | SecurityException e){
+            Log.e("Error", e.toString());
+        }
+        imgRef.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Log.d("Image link", taskSnapshot.getTask().getResult().toString());
+            }
+        });
+    }
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 }
