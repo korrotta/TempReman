a6b730be3650e7547dd08d7235a9246240e2751e
21120606 <hoaihieulam@gmail.com>
2024-01-14 21:33:57 +0700

    Fix Table Detail Inuse UI

--- src/app/src/main/java/com/softwareengineering/restaurant/StaffPackage/TableDetailInuse.java
+++ src/app/src/main/java/com/softwareengineering/restaurant/StaffPackage/TableDetailInuse.java
@@ -6,20 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.softwareengineering.restaurant.R;
+import com.softwareengineering.restaurant.databinding.ActivityTableDetailInuseBinding;
+import com.squareup.picasso.Picasso;
 
 public class TableDetailInuse extends AppCompatActivity {
-    Button btn_order;
+
+    private ImageView topMenuImg;
+    private TextView topMenuName;
+    private ActivityTableDetailInuseBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_table_detail_inuse);
+        binding = ActivityTableDetailInuseBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        btn_order = findViewById(R.id.btn_order);
+        topMenuImg = findViewById(R.id.topMenuImg);
+        topMenuName = findViewById(R.id.topMenuName);
 
-        btn_order.setOnClickListener(new View.OnClickListener() {
+        initToolBar();
+
+        // Big table ID Display
+        binding.tableDetailInuseTableID.setText("1");
+
+        // Info box
+        binding.tableDetailsInuseCustomerImg.setImageResource(R.drawable.default_user);
+        binding.tableDetailsInuseCustomerName.setText("Name");
+        binding.tableDetailsInuseNumberTable.setText("1");
+        binding.tableDetailsInuseDate.setText("01/01/2024");
+        binding.tableDetailsInuseTime.setText("20:00");
+
+        // Order Button
+        binding.btnOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(TableDetailInuse.this, StaffOrderActivity.class);
@@ -27,5 +49,27 @@ public class TableDetailInuse extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Cancel Button
+        binding.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void initToolBar() {
+        topMenuImg.setImageResource(R.drawable.back);
+
+        topMenuImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        topMenuName.setText(R.string.table_detail);
     }
 }
