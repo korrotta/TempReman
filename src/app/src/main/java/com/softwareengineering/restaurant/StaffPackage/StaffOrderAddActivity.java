0110b34d667721b1b8bcda27fa178aaaae707ca2
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2024-01-18 21:51:34 +0700

    Order continue

--- src/app/src/main/java/com/softwareengineering/restaurant/StaffPackage/StaffOrderAddActivity.java
+++ src/app/src/main/java/com/softwareengineering/restaurant/StaffPackage/StaffOrderAddActivity.java
@@ -2,11 +2,16 @@ package com.softwareengineering.restaurant.StaffPackage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListView;
 
 import com.softwareengineering.restaurant.ItemClasses.MenuItem;
+import com.softwareengineering.restaurant.ItemClasses.OrderItem;
 import com.softwareengineering.restaurant.R;
 
 import java.util.ArrayList;
@@ -14,12 +19,14 @@ import java.util.List;
 
 public class StaffOrderAddActivity extends AppCompatActivity {
     private GridView list_menu;
+    private Button btn_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_order_add);
 
+        btn_add = findViewById(R.id.btn_add);
         list_menu = findViewById(R.id.list_menu);
 
         // Tạo danh sách MenuItem (giả sử bạn đã có dữ liệu)
@@ -31,5 +38,19 @@ public class StaffOrderAddActivity extends AppCompatActivity {
         // Tạo Adapter và thiết lập cho GridView
         MenuAdapter menuAdapter = new MenuAdapter(this, menuItems);
         list_menu.setAdapter(menuAdapter);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StaffOrderAddActivity.this, StaffOrderActivity.class);
+                // Lấy danh sách món ăn đã chọn từ Adapter
+                List<OrderItem> listFoodSelected = menuAdapter.getSelectedItems();
+
+                intent.putParcelableArrayListExtra("listFoodSelected", new ArrayList<>(listFoodSelected));
+                startActivity(intent);
+
+                finish();
+            }
+        });
     }
 }
