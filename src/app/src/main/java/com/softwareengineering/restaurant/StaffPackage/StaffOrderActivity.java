3cb45be02013cb2a9da8a7ec331866889779d8dd
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2024-01-19 18:20:04 +0700

    Show dailog order and edit layout order

--- src/app/src/main/java/com/softwareengineering/restaurant/StaffPackage/StaffOrderActivity.java
+++ src/app/src/main/java/com/softwareengineering/restaurant/StaffPackage/StaffOrderActivity.java
@@ -2,11 +2,16 @@ package com.softwareengineering.restaurant.StaffPackage;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
@@ -125,11 +130,32 @@ public class StaffOrderActivity extends AppCompatActivity {
                         FirebaseFirestore.getInstance().collection("table").document(final_tableID[0]).update("quantityList", new ArrayList<String>());
                         FirebaseFirestore.getInstance().collection("table").document(final_tableID[0]).update("state", "idle");
                         FirebaseFirestore.getInstance().collection("table").document(final_tableID[0]).update("userinuse", "");
-                        finish();
+                        showSuccessDialog();
                     }
                 });
             }
         });
+    }
+
+    private void showSuccessDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(StaffOrderActivity.this);
+
+        // Create a TextView with custom text color
+        TextView messageTextView = new TextView(StaffOrderActivity.this);
+        messageTextView.setText("You have successfully paid!");
+        messageTextView.setTextColor(Color.parseColor("#6AC259"));
+        messageTextView.setTextSize(24);
+        messageTextView.setGravity(Gravity.CENTER);
+        messageTextView.setPadding(16, 16, 16, 16);
+
+        builder.setMessage(" ")
+                .setView(messageTextView)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .show();
     }
 
     private void setDataForTextView(){
